b47c7c694d375b0a1d49de9f891251c648b7cefa
BillS <will.staf@gmail.com>
2022-06-07 21:25:19 +0000

    removing react from game

--- game-app/src/index.js
+++ game-app/src/index.js
@@ -1,24 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 import ShooterGame from './Game';
 import * as conf from './Config';
 
 const tileSize = 25;
 
-new ShooterGame(conf.config, tileSize);//.doGame();
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+new ShooterGame(conf.config, tileSize);
